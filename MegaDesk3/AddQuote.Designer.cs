e17085e02277b4f452fa123eb5cb60669acf7b88
danverde <gre12014@byui.edu>
2018-05-10 08:11:38 -0600

    Setting up main menu. renamed the forms

--- MegaDesk3/AddQuote.Designer.cs
+++ MegaDesk3/AddQuote.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // AddQuote
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AddQuote";
+            this.Name = "AddQuote";
+            this.Text = "Add Quote";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- MegaDesk3/MainMenu.Designer.cs
+++ MegaDesk3/MainMenu.Designer.cs
@@ -28,13 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenu));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.exit = new System.Windows.Forms.Button();
+            this.searchQuotes = new System.Windows.Forms.Button();
+            this.viewQuote = new System.Windows.Forms.Button();
+            this.newQuote = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Location = new System.Drawing.Point(385, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(364, 316);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // exit
+            // 
+            this.exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exit.Location = new System.Drawing.Point(44, 307);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(243, 56);
+            this.exit.TabIndex = 8;
+            this.exit.Text = "Exit";
+            this.exit.UseVisualStyleBackColor = true;
+            // 
+            // searchQuotes
+            // 
+            this.searchQuotes.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchQuotes.Location = new System.Drawing.Point(44, 217);
+            this.searchQuotes.Name = "searchQuotes";
+            this.searchQuotes.Size = new System.Drawing.Size(243, 56);
+            this.searchQuotes.TabIndex = 7;
+            this.searchQuotes.Text = "Search Quotes";
+            this.searchQuotes.UseVisualStyleBackColor = true;
+            // 
+            // viewQuote
+            // 
+            this.viewQuote.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewQuote.Location = new System.Drawing.Point(44, 131);
+            this.viewQuote.Name = "viewQuote";
+            this.viewQuote.Size = new System.Drawing.Size(243, 56);
+            this.viewQuote.TabIndex = 6;
+            this.viewQuote.Text = "View Quotes";
+            this.viewQuote.UseVisualStyleBackColor = true;
+            // 
+            // newQuote
+            // 
+            this.newQuote.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newQuote.Location = new System.Drawing.Point(44, 47);
+            this.newQuote.Name = "newQuote";
+            this.newQuote.Size = new System.Drawing.Size(243, 56);
+            this.newQuote.TabIndex = 5;
+            this.newQuote.Text = "Add New Quote";
+            this.newQuote.UseVisualStyleBackColor = true;
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.searchQuotes);
+            this.Controls.Add(this.viewQuote);
+            this.Controls.Add(this.newQuote);
+            this.Name = "MainMenu";
+            this.Text = "Main Menu";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Button searchQuotes;
+        private System.Windows.Forms.Button viewQuote;
+        private System.Windows.Forms.Button newQuote;
     }
 }
 
