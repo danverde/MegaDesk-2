b762d09a07a87065ac0d62d85e56f6ea73ee8bcb
danverde <gre12014@byui.edu>
2018-05-19 17:01:43 -0600

    view all quotes working

--- MegaDesk4/ViewAllQuotes.Designer.cs
+++ MegaDesk4/ViewAllQuotes.Designer.cs
@@ -28,13 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.CloseViewAllQuotesButton = new System.Windows.Forms.Button();
+            this.viewAll = new System.Windows.Forms.DataGridView();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MessageLabel = new System.Windows.Forms.Label();
+            this.surfaceBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.viewAll)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.surfaceBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // CloseViewAllQuotesButton
             // 
             this.CloseViewAllQuotesButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CloseViewAllQuotesButton.Location = new System.Drawing.Point(688, 398);
+            this.CloseViewAllQuotesButton.Location = new System.Drawing.Point(744, 398);
             this.CloseViewAllQuotesButton.Name = "CloseViewAllQuotesButton";
             this.CloseViewAllQuotesButton.Size = new System.Drawing.Size(100, 40);
             this.CloseViewAllQuotesButton.TabIndex = 10;
@@ -42,21 +56,107 @@
             this.CloseViewAllQuotesButton.UseVisualStyleBackColor = true;
             this.CloseViewAllQuotesButton.Click += new System.EventHandler(this.CloseViewAllQuotesButton_Click);
             // 
+            // viewAll
+            // 
+            this.viewAll.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.viewAll.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column2,
+            this.Column1,
+            this.Price,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.viewAll.Location = new System.Drawing.Point(0, 12);
+            this.viewAll.Name = "viewAll";
+            this.viewAll.Size = new System.Drawing.Size(844, 366);
+            this.viewAll.TabIndex = 11;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Order Date";
+            this.Column2.Name = "Column2";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Customer Name";
+            this.Column1.Name = "Column1";
+            // 
+            // Price
+            // 
+            this.Price.HeaderText = "Price";
+            this.Price.Name = "Price";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Delivery Time";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Material";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Drawers";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Width";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Depth";
+            this.Column7.Name = "Column7";
+            // 
+            // MessageLabel
+            // 
+            this.MessageLabel.AutoSize = true;
+            this.MessageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MessageLabel.Location = new System.Drawing.Point(13, 398);
+            this.MessageLabel.Name = "MessageLabel";
+            this.MessageLabel.Size = new System.Drawing.Size(0, 29);
+            this.MessageLabel.TabIndex = 12;
+            // 
+            // surfaceBindingSource
+            // 
+            this.surfaceBindingSource.DataSource = typeof(MegaDesk4.Desk.Surface);
+            // 
             // ViewAllQuotes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(857, 450);
+            this.Controls.Add(this.MessageLabel);
+            this.Controls.Add(this.viewAll);
             this.Controls.Add(this.CloseViewAllQuotesButton);
             this.Name = "ViewAllQuotes";
             this.Text = "View Quotes";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ViewAllQuotes_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.viewAll)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.surfaceBindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button CloseViewAllQuotesButton;
+        private System.Windows.Forms.DataGridView viewAll;
+        private System.Windows.Forms.Label MessageLabel;
+        private System.Windows.Forms.BindingSource surfaceBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
     }
 }
--- MegaDesk4/ViewAllQuotes.cs
+++ MegaDesk4/ViewAllQuotes.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,7 @@ namespace MegaDesk4
         public ViewAllQuotes()
         {
             InitializeComponent();
+            ReadQuotes();
         }
 
         private void CloseViewAllQuotesButton_Click(object sender, EventArgs e)
@@ -28,6 +30,34 @@ namespace MegaDesk4
         {
             MainMenu mainMenu = (MainMenu)Tag;
             mainMenu.Show();
+        }
+
+        private void ReadQuotes()
+        {
+            var fileName = "quotes.csv";
+            string[] quotes;
+
+            if (File.Exists(fileName))
+            {
+                try
+                {
+                    quotes = File.ReadAllLines(fileName);
+                    for (int i = 1; i < quotes.Length; i++)
+                    {
+                        // this has got to be an awful way to do this...
+                        var quote = quotes[i].Split(',').ToArray();
+                        viewAll.Rows.Add(quote[0], quote[1], quote[2], quote[3], quote[4], quote[5], quote[6], quote[7]);
+                    }
+                }
+                catch (Exception Err)
+                {
+                    MessageLabel.Text = Err.Message;
+                }
+            }
+            else
+            {
+                MessageLabel.Text = "Unable to find quotes file";
+            }
         }
     }
 }
